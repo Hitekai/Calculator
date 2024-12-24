10b2b7d539f3b4a7916ece97c181464e4afa5a69
Hitekai <rabotrpn@gmail.com>
2024-12-24 13:39:10 +0300

    Добавлен код
    Добавлен код

--- README.md
+++
@@ -1,2 +0,0 @@
-# Calculator
- Тестовая работа на Python
---
+++ main.py
@@ -0,0 +1,19 @@
+operand1 = float(input("Введите первый операнд: "))
+operator = input("Введите оператор (+, -, *, /): ")
+operand2 = float(input("Введите второй операнд: "))
+
+if operator == '+':
+    result = operand1 + operand2
+elif operator == '-':
+    result = operand1 - operand2
+elif operator == '*':
+    result = operand1 * operand2
+elif operator == '/':
+    if operand2 != 0:
+        result = operand1 / operand2
+    else:
+        result = "Ошибка: Деление на ноль!"
+else:
+    result = "Ошибка: Неподдерживаемый оператор!"
+
+print(f"Результат: {result}")
